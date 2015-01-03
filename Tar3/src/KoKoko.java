b9d043564ecf16416d41bfdd999da9acd7138292
OrMan <OrMan@OrMan8>
2015-01-03 20:36:17 +0200

    prerty prnt

--- Tar3/src/KoKoko.java
+++ Tar3/src/KoKoko.java
@@ -24,12 +24,15 @@ public class KoKoko {
 	public void setX1(int x1) {
 		this.x1 = x1;
 	}
+
 	public void setX2(int x2) {
 		this.x2 = x2;
 	}
+
 	public int getX2() {
 		return x2;
 	}
+
 	private void get() {
 		// TODO Auto-generated method stub
 
