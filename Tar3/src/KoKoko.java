d2c3b8b94b70b66b4dfd69d4ad5f1566d212400d
OrMan <OrMan@OrMan8>
2015-01-03 20:35:36 +0200

    nwe funk

--- Tar3/src/KoKoko.java
+++ Tar3/src/KoKoko.java
@@ -8,5 +8,21 @@ public class KoKoko {
 		this.x1 = 1;
 		// TODO: find new
 		// todo: loo
+
+		SetX1(this.x1);
 	}
+
+	private void SetX1(int x12) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int getX1() {
+		return x1;
+	}
+
+	public void setX1(int x1) {
+		this.x1 = x1;
+	}
+
 }
